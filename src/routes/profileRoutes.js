f6a2aca8e70f9f9300a372a9fc4fe8359ff77636
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-06-30 01:36:33 +0500

    fixing profile routes bug

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -6,6 +6,6 @@ const upload = require("../middleware/upload");
 const {CreateProfile, userProfile} = require("../controllers/profile");
 
 router.post("/create-profile", limiter, upload.single("Image"), authMiddleware, CreateProfile);
-router.post("/user-profile", limiter, authMiddleware, userProfile);
+router.get("/user-profile", limiter, authMiddleware, userProfile);
 
 module.exports = router;
