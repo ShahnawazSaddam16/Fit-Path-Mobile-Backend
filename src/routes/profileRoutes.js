a3f02b23444db05d937f6ab4e433692537e96e7f
ShahnawazSaddam16 <saddammaqsood4321@gmail.com>
2026-06-30 20:52:31 +0500

    now adding editProfile in routes

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const limiter = require("../utils/limiter");
 const authMiddleware = require("../middleware/authMiddleware");
 const upload = require("../middleware/upload");
-const {CreateProfile, userProfile} = require("../controllers/profile");
+const {CreateProfile, userProfile, editProfile} = require("../controllers/profile");
 
 router.post("/create-profile", limiter, upload.single("Image"), authMiddleware, CreateProfile);
 router.get("/user-profile", limiter, authMiddleware, userProfile);
+router.put("/edit-profile", limiter, authMiddleware, editProfile);
 
 module.exports = router;
